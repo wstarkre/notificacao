ccc6db228353fc9f7e61bc797529c8141a4e3ae5
Sabrina <sabrinavitreis@gmail.com>
2026-07-14 20:55:45 -0300

    commit inicial

---
+++ src/test/java/com/javanauta/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
